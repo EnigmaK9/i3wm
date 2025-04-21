29339f4ce70c66f7a9272ddeb0161c52858be06f
Carlos Padilla <enigmak9@protonmail.com>
2025-04-21 05:27:19 -0600

    current version of my repository

---
+++ scripts/set_wallpaper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+case "$1" in
+  1) feh --bg-scale ~/.config/i3/wallpapers/01wallpaper.jpg ;;
+  2) feh --bg-scale ~/.config/i3/wallpapers/02wallpaper.jpg ;;
+  3) feh --bg-scale ~/.config/i3/wallpapers/03wallpaper.jpeg ;;
+  4) feh --bg-scale ~/.config/i3/wallpapers/04wallpaper.jpg ;;    
+  5) feh --bg-scale ~/.config/i3/wallpapers/05wallpaper.jpg ;;
+  6) feh --bg-scale ~/.config/i3/wallpapers/06wallpaper.png ;;
+  7) feh --bg-scale ~/.config/i3/wallpapers/07wallpaper.jpg ;;  
+  8) feh --bg-scale ~/.config/i3/wallpapers/08wallpaper.png ;;
+  9) feh --bg-scale ~/.config/i3/wallpapers/09wallpaper.jpg ;;
+  10)feh --bg-scale ~/.config/i3/wallpapers/04wallpaper.png ;;
+esac
+
