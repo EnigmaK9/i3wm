a8cc9439d9ca6fdc3c3c2e1a4a06a66a0d6bd22a
Carlos Padilla <enigmak9@protonmail.com>
2025-05-10 06:15:06 -0600

    updated wallpapers

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -2,13 +2,13 @@
 case "$1" in
   1) feh --bg-scale ~/.config/i3/wallpapers/01wallpaper.jpg ;;
   2) feh --bg-scale ~/.config/i3/wallpapers/02wallpaper.jpg ;;
-  3) feh --bg-scale ~/.config/i3/wallpapers/03wallpaper.jpeg ;;
-  4) feh --bg-scale ~/.config/i3/wallpapers/04wallpaper.png ;;    
-  5) feh --bg-scale ~/.config/i3/wallpapers/05wallpaper.jpg ;;
+  3) feh --bg-scale ~/.config/i3/wallpapers/03wallpaper.jpg     ;;
+  4) feh --bg-scale ~/.config/i3/wallpapers/04wallpaper.jpg ;;    
+  5) feh --bg-scale ~/.config/i3/wallpapers/05wallpaper.png ;;
   6) feh --bg-scale ~/.config/i3/wallpapers/06wallpaper.png ;;
   7) feh --bg-scale ~/.config/i3/wallpapers/07wallpaper.jpg ;;  
-  8) feh --bg-scale ~/.config/i3/wallpapers/08wallpaper.jpg ;;
+  8) feh --bg-scale ~/.config/i3/wallpapers/08wallpaper.png ;;
   9) feh --bg-scale ~/.config/i3/wallpapers/09wallpaper.png ;;
-  10)feh --bg-scale ~/.config/i3/wallpapers/09wallpaper.jpg ;;
+  10)feh --bg-scale ~/.config/i3/wallpapers/10wallpaper.png ;;
 esac
 
